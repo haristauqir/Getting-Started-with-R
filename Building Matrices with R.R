76fcba0dbd06e0e5a191ca605692017988415311
haristauqir <haristauqir@gmail.com>
2017-07-25 01:47:10 +0500

    Lesson 1 - 3
    Introduction to R - Implementation of concepts in Lesson 1 to 3 of datacamp.com

---
+++ Building Matrices with R.R
@@ -0,0 +1,20 @@
+# create vector of company details
+company1 <- c("March 2016", "Global")
+company2 <- c("August 2016", "Middle East - Kuwait")
+
+# Construct matrix
+companies_matrix <- matrix(c(company1, company2), nrow = 2, byrow = TRUE)
+
+# Vectors my_column_names and my_row_names, used for naming
+my_column_names <- c("Starting Date", "Regions")
+my_row_names <- c("Cloud Stem Ltd", "MyAdminSoft Limited")
+
+# Name the columns with my_column_names
+colnames(companies_matrix) <- my_column_names
+
+# Name the rows with company_names
+rownames(companies_matrix) <- my_row_names
+
+# Print out companies_matrix
+companies_matrix
+
